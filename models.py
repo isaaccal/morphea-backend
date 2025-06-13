d5b8b68c896a4edccd8642a153fbc31fa4c11855
isaaccal <isaaccal@gmail.com>
2025-06-13 13:11:39 -0600

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
 class User(Base):
@@ -7,3 +8,17 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(255), unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
+
+    subscriptions = relationship("Subscription", back_populates="user")
+
+
+class Subscription(Base):
+    __tablename__ = "subscriptions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    plan_name = Column(String, default="gratis")
+    dreams_allowed = Column(Integer, default=1)
+    dreams_used = Column(Integer, default=0)
+
+    user = relationship("User", back_populates="subscriptions")
