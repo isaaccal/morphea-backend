2f168cd0fca6c7e815531fa4a35bc5c8fec7cac5
isaaccal <isaaccal@gmail.com>
2025-06-13 12:10:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -125,3 +125,5 @@ async def interpretar_sueno(data: DreamRequest):
         "message": "Interpretación enviada",
         "status": "success"
     }
+from auth import router as auth_router
+app.include_router(auth_router)
